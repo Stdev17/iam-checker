fea51b49b920943e9d32a2f17cd9699a27d727d4
Beryl Leta <contact@berylleta.dev>
2022-02-27 20:43:05 +0900

    add: check whether the profile has expired

--- main.go
+++ main.go
@@ -2,30 +2,38 @@ package main
 
 import (
     "context"
-    "fmt"
     "log"
+    "time"
     "github.com/aws/aws-sdk-go-v2/aws"
     "github.com/aws/aws-sdk-go-v2/config"
     "github.com/aws/aws-sdk-go-v2/service/iam"
 )
 
+type IAMProfile struct {
+    accessKeyId string
+    userName string
+    createdDate time.Time
+}
+
 func main() {
-    err := FetchIAM()
+    _, err := FetchIAM()
     if err != nil {
         //
         return
     }
+
+    return
     //for _, obj := range results {
     //    log.Print(obj.(string))
     //}
 }
 
 // FetchIAM connects to AWS SDK API with an IAM access key pair and fetch the IAM access key data from its account.
-func FetchIAM() (error) {
+func FetchIAM() ([]IAMProfile, error) {
     cfg, err := config.LoadDefaultConfig(context.TODO())
     if err != nil {
         log.Fatal(err)
-        return err
+        return nil, err
     }
 
     client := iam.NewFromConfig(cfg)
@@ -37,12 +45,31 @@ func FetchIAM() (error) {
     fetched, err := client.ListAccessKeys(context.TODO(), input)
     if err != nil {
         log.Fatal(err)
-        return err
+        return nil, err
     }
+
+    var IAMs []IAMProfile
 
     for _, key := range fetched.AccessKeyMetadata {
-        fmt.Println("Status for access key " + *key.AccessKeyId + ": " + string(*key.UserName) + ", " + (*key.CreateDate).String())
+        tmp := IAMProfile{
+            accessKeyId: *key.AccessKeyId,
+            userName: string(*key.UserName),
+            createdDate: *key.CreateDate,
+        }
+        IAMs = append(IAMs, tmp)
     }
 
-    return nil
+    return IAMs, nil
+}
+
+// CheckProfileExpired filters out the valid profiles from the originally fetched data.
+func CheckProfileExpired(hour time.Duration, given []IAMProfile) ([]IAMProfile) {
+    var filtered []IAMProfile
+    for _, val := range given {
+        if val.createdDate.Add(hour).Before(time.Now()) {
+            filtered = append(filtered, val)
+        }
+    }
+
+    return filtered
 }
--- main_test.go
+++ main_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"time"
     "testing"
     "github.com/stretchr/testify/assert"
 )
 
 func TestFetchIAM(t *testing.T) {
     // connect to the AWS API with the access key pair
-    assert.Equal(t, nil, FetchIAM(), "Failed to fetch IAM profiles")
+	fetched, err := FetchIAM()
+    assert.Equal(t, nil, err, "Failed to fetch IAM profiles")
     // find out IAM access keys over N hours
+	hours := time.Duration(time.Hour * 720)
+	assert.Equal(t, 1, len(CheckProfileExpired(hours, fetched)), "Counts of targeted IAM profiles are not the same as expected")
     // save a file which contains the suggested result
 }
